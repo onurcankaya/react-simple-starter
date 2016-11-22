f6aafff7f62e203b704597592dcc6ed224f02829
Onur Cankaya <oecankaya@gmail.com>
2016-11-22 17:31:11 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React simple starter</h1>
+      </div>
+    );
+  }
+}
